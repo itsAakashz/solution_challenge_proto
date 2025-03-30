4ff3bf437239d444fd46bbf6043c6484b2ad0e18
Aakash Kumar <gzatrop@gmail.com>
2025-03-30 15:40:41 +0530

    Update home_screen.dart

--- soilgpt/lib/home_screen.dart
+++ soilgpt/lib/home_screen.dart
@@ -225,7 +225,7 @@ Suggest the best crop(s) along with reasons.
             ),
             _buildDrawerItem(Icons.home, 'Home', () => Navigator.pop(context)),
             _buildDrawerItem(Icons.cast_for_education, 'AgriEdu',
-                    () => Navigator.push(context, MaterialPageRoute(builder: (context) => ShortVideoFeed()))),
+                    () => Navigator.push(context, MaterialPageRoute(builder: (context) => AgriEduScreen()))),
             _buildDrawerItem(Icons.camera_alt, 'Soil Lens',
                     () => Navigator.push(context, MaterialPageRoute(builder: (context) => SoilLensScreen()))),
             _buildDrawerItem(Icons.shopping_cart_rounded, 'Mandi Price',
